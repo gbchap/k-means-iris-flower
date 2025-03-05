7678728676f314a15d2428a9654c3e54b63e772e
gbchap <gb.freitaschap@gmail.com>
2025-03-04 23:13:22 -0300

    3 and 4

--- projeto/inicial.cpp
+++ projeto/inicial.cpp
@@ -5,9 +5,72 @@ using namespace std;
 
 #define N 6
 #define M 2
+#define k 2
 
-void k_means(double x[][M], int k){
+
+void atribuicao(float dist[N][k], int att[N], double x[N][M], float cluster[k][M]){
+    //agora veremos a proximidade de cada um
+    //dos outros pontos em relação a cada centróide.
+    //Assim, vamos atribuir o ponto ao centróide mais próximo
+    //utilizando dist. Euclidiana.
+
+    for(int c=0; c<k; c++){
+        for(int i=0; i<N; i++){
+            float x1 = x[i][0];
+            float y1 = x[i][1];
+            float x2 = cluster[c][0];
+            float y2 = cluster[c][1];
+            dist[i][c] = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
+        }
+    }
+    
+    //atribuição de cada dado ao cluster mais proximo
+    //(comparar com as outras distancias).
+
+    for(int c=0; c<k; c+=2){
+        for(int i=0; i<N; i++){
+            if(dist[i][c] < dist[i][c+1]){
+                att[i] = c;
+            }else{
+                att[i] = c+1;
+            }
+        }
+    }
+    // assim, se att[i] = 0, indica que o ponto
+    // está mais próximo da cluster 1. Se att[i] = 1,
+    // o ponto está mais próximo da segunda cluster.
+}
+
+void atualizarCentroides(float cluster[k][M], double x[N][M], int att[N]){
+    //calculo da media da cluster:
+    float soma[k][M] = {0};
+    float media[k][M] = {0};
+    for(int c=0; c<k; c++){
+        int num = 0;
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                if (att[i] == c){
+                    soma[c][j] += x[i][j];
+                    num++;
+                }
+            }
+        }
+        for(int j=0; j<M; j++){
+            media[c][j] = soma[c][j]/(num/M);
+        }
+    }
+    
+    for(int c=0; c<k; c++){
+        for(int i=0; i<M; i++){
+            cluster[c][i] = media[c][i];
+            //cout << cluster[c][i] << " ";
+        }
+    }
+}
+
+void k_means(double x[][M]){
     cout << fixed << setprecision(2);
+
     //Primeiramente, vamos definir k clusters de maneira
     //aleatória. Para isso, criaremos um algoritmo que possa
     //espaçar essa definição de acordo com a ordem
@@ -30,56 +93,38 @@ void k_means(double x[][M], int k){
         }
     }
 
-    //agora veremos a proximidade de cada um
-    //dos outros pontos em relação a cada centróide.
-    //Assim, vamos atribuir o ponto ao centróide mais próximo
-    //utilizando dist. Euclidiana.
+    //As clusters serão atualizadas para a média.
+    //O que nos resta fazer é repetir os passos 2 e 3
+    //durante um número de iterações aleatório, como,
+    //por exemplo, 1000.
+
+    //Temos como passo 2 a função atribuicao e 
+    //como passo 3 a função atualizarCentroides.
 
     float dist[N][k];
-    //comparar N de cada k depois;
-
-    float dist[N][k];
-
-    for(int c=0; c<k; c++){
-        //cout << endl;
-        for(int i=0; i<N; i++){
-            float x1 = x[i][0];
-            float y1 = x[i][1];
-            float x2 = cluster[c][0];
-            float y2 = cluster[c][1];
-            dist[i][c] = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
-            //cout << dist[i][c] << " ";
-        }
-    }
-    //atribuição de cada dado ao cluster mais proximo
-    //(comparar com as outras distancias).
     int att[k];
 
-    for(int c=0; c<k; c+=2){
-        for(int i=0; i<N; i++){
-            if(dist[i][c] < dist[i][c+1]){
-                att[i] = c;
-            }else{
-                att[i] = c+1;
-            }
-        }
+    for(int iter = 0; iter < 1000; iter++){
+        atribuicao(dist, att, x, cluster);
+        atualizarCentroides(cluster, x, att);
     }
-    // assim, se att[i] = 0, indica que o ponto
-    // está mais próximo da cluster 1. Se at[i] = 2,
-    // o ponto está mais próximo da segunda cluster.
+    for(int i=0; i<N; i++){
+        cout<< fixed << setprecision(0);
+        cout << "Ponto (" << x[i][0] << ", " << x[i][1] << "): " << att[i];
+        cout<< endl;
+    }
 }
 
 int main()
 {
     double x[N][M] = {{1, 2}, {2, 3}, {3, 4}, {6, 5}, {7, 6}, {8, 7}};
-    int k = 2;
     for (int i = 0; i < N; i++){
         for (int j = 0; j < M; j++)
             cout << x[i][j] << ' ';
         cout << endl;
     }
             
-    k_means(x, k);
+    k_means(x);
     
     return 0;
 }
--- test.cpp
+++ test.cpp
@@ -6,7 +6,7 @@ using namespace std;
 #define N 6
 #define M 2
 int main(){
-    cout << fixed << setprecision(2);
+    cout << fixed << setprecision(1);
     int k = 2;
     double x[N][M] = {{1, 2}, {2, 3}, {3, 4}, {6, 5}, {7, 6}, {8, 7}};
     float cluster[k][M];
@@ -23,14 +23,14 @@ int main(){
     float dist[N][k];
 
     for(int c=0; c<k; c++){
-        cout<< endl;
+        //cout<< endl;
         for(int i=0; i<N; i++){
             float x1 = x[i][0];
             float y1 = x[i][1];
             float x2 = cluster[c][0];
             float y2 = cluster[c][1];
             dist[i][c] = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
-            cout << dist[i][c] << " ";
+            //cout << dist[i][c] << " ";
         }
     }
 
@@ -44,7 +44,38 @@ int main(){
             }
         }
     }
-    //for(int i=0; i<N; i++){
-       //cout << att[i] << " ";
-    //}
+    for(int i=0; i<N; i++){
+        //cout << att[i] << " ";
+    }
+
+    float soma[k][M] = {0};
+    float media[k][M] = {0};
+    for(int c=0; c<k; c++){
+        int num = 0;
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                if (att[i] == c){
+                    soma[c][j] += x[i][j];
+                    num++;
+                }
+            }
+        }
+        for(int j=0; j<M; j++){
+            media[c][j] = soma[c][j]/(num/M);
+        }
+    }
+    for(int c=0; c<k; c++){
+        for(int j=0; j<M; j++){
+            //cout << media[c][j] << " ";
+        }
+        //cout << endl;
+    }
+    for(int c=0; c<k; c++){
+        for(int i=0; i<M; i++){
+            cluster[c][i] = media[c][i];
+            cout << cluster[c][i] << " ";
+        }
+        cout << endl;
+    }
 }
+
